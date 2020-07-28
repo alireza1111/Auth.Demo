aa1e772b358179a35d50f8da3cec558c83c9c943
alireza1111 <alireza.davoudian@snd.gu.se>
2020-07-28 15:32:41 +0200

    Add project files.

---
+++ Auth.Demo/IJwtAuthenticationManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Demo
+{
+    public interface IJwtAuthenticationManager
+    {
+        string AuthenticatedEncryptorConfiguration(string username, string password);
+    }
+}
